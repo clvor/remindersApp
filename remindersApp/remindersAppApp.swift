909243cdb4ad1caea6e5384945a6b94a4b845379
clover <atkinsonclover4@gmail.com>
2023-06-27 09:33:37 -0600

    Initial Commit

---
+++ remindersApp/remindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindersAppApp.swift
+//  remindersApp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct remindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
